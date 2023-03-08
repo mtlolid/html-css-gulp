b921fb38b8b4a0babeff84e7c4de8f8ac8bd2e2c
mtlolid <mtloidmail@gmail.com>
2023-03-08 20:36:35 +0200

    new__commit

---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+const gulp = require('gulp');
+const { series, parallel } = require('gulp');
+const pug = require('gulp-pug');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cssnano = require('gulp-cssnano');
+const rename = require("gulp-rename");
+const imagemin = require('gulp-imagemin');
+const browserSync = require('browser-sync').create();
+const del = require('del');
+
+const html = () => {
+    return gulp.src('src/pug/*.pug')
+        .pipe(pug({ pretty: true }))
+        .pipe(gulp.dest('build'))
+}
+
+const styles = () => {
+    return gulp.src('src/styles/*.scss')
+        .pipe(sass().on('err', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(cssnano())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('build/css'))
+}
+
+const images = () => {
+    return gulp.src('src/images/*.*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('build/images'))
+}
+
+const fonts = () => {
+    return gulp.src('src/fonts/*.*')
+        .pipe(gulp.dest('build/fonts'))
+}
+
+const server = () => {
+    browserSync.init({
+        server: {
+            baseDir: './build'
+        },
+        notify: false
+    });
+    browserSync.watch('build', browserSync.reload)
+}
+
+const deleteBuild = (cb) => {
+    return del('build/**/*.*').then(() => { cb() })
+}
+
+const watch = () => {
+    gulp.watch('src/pug/**/*.pug', html);
+    gulp.watch('src/styles/**/*.scss', styles);
+    gulp.watch('src/images/*.*', images);
+    gulp.watch('src/fonts/*.*', fonts);
+}
+
+exports.default = series(
+    deleteBuild,
+    parallel(html, fonts, styles, images),
+    parallel(watch, server)
+)
